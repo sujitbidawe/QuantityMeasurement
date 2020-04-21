4ba41b6ccf5a8cbffffc3097c7e8e4e08b29620b
sujitbidawe <bidawesujit@gmail.com>
2020-04-21 22:14:03 +0530

    added: test case to check when given null value for feet

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -18,4 +18,11 @@ public class QuantityTest {
         Feet feet2 = new Feet(1.0);
         Assert.assertNotEquals(feet1, feet2);
     }
+
+    @Test
+    public void given0FeetAndNullValue_shouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assert.assertNotEquals(feet1, feet2);
+    }
 }
