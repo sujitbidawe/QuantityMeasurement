2ac1a56bcbead004c4beebe56594f4203a4fcc95
sujitbidawe <bidawesujit@gmail.com>
2020-04-21 22:51:53 +0530

    added: test code to check equqlity and inequality of references of inch and feet

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -13,6 +13,12 @@ public class QuantityTest {
     }
 
     @Test
+    public void givenSameFeetReference_shouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet1);
+    }
+
+    @Test
     public void given0FeetAnd1Feet_shouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
@@ -34,6 +40,12 @@ public class QuantityTest {
     }
 
     @Test
+    public void givenSameInchReference_shouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Assert.assertEquals(inch1, inch1);
+    }
+
+    @Test
     public void given0InchAnd1Inch_shouldReturnNotEqual() {
         Inch inch1 = new Inch(0.0);
         Inch inch2 = new Inch(1.0);
@@ -44,6 +56,13 @@ public class QuantityTest {
     public void given0InchAndNullValue_shouldReturnNotEqual() {
         Inch inch1 = new Inch(0.0);
         Inch inch2 = null;
-        Assert.assertNotEquals(inch1, null);
+        Assert.assertNotEquals(inch1, inch2);
+    }
+
+    @Test
+    public void givenValuesWithDifferentReference_shouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Feet feet1 = new Feet(0.0);
+        Assert.assertNotEquals(inch1, feet1);
     }
 }
