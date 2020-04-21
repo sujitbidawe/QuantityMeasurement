1e95738b0bc8f9bd26d5eb62aef4b422e7b0a066
sujitbidawe <bidawesujit@gmail.com>
2020-04-21 22:22:51 +0530

    added: test case to check unequality when given null value for inch

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -39,4 +39,11 @@ public class QuantityTest {
         Inch inch2 = new Inch(1.0);
         Assert.assertNotEquals(inch1, inch2);
     }
+
+    @Test
+    public void given0InchAndNullValue_shouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = null;
+        Assert.assertNotEquals(inch1, null);
+    }
 }
