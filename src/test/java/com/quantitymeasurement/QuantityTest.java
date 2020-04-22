e6de8ec3629e69cdde83dc6826b765c27e7d766a
sujitbidawe <bidawesujit@gmail.com>
2020-04-22 23:26:48 +0530

    added: test case to add 1 feet and 1 feet

--- src/test/java/com/quantitymeasurement/QuantityTest.java
+++ src/test/java/com/quantitymeasurement/QuantityTest.java
@@ -199,4 +199,12 @@ public class QuantityTest {
         double sum = feet1.add(inch2);
         Assert.assertEquals(14.0, sum, 0.0);
     }
+
+    @Test
+    public void given1FeetAnd1Feet_whenAdded_shouldReturnSum() {
+        Length feet1 = new Length(Unit.FEET, 1);
+        Length feet2 = new Length(Unit.FEET, 1);
+        double sum = feet1.add(feet2);
+        Assert.assertEquals(24, sum, 0.0);
+    }
 }
