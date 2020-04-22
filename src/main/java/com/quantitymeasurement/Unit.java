30f5d12eb655056f7c0752d7c103155ed56f6bf0
sujitbidawe <bidawesujit@gmail.com>
2020-04-22 21:58:56 +0530

    added: code for enum of Unit

---
+++ src/main/java/com/quantitymeasurement/Unit.java
@@ -0,0 +1,18 @@
+package com.quantitymeasurement;
+
+public enum Unit {
+
+    FEET(12.0), INCH(1.0), YARD(36);
+
+    private final double baseUnitComparison;
+
+    Unit(double baseUnitComparison){
+        this.baseUnitComparison = baseUnitComparison;
+    }
+
+    public boolean compare(Length l1, Length l2){
+        return Double.compare(l1.value * l1.unit.baseUnitComparison,
+                              l2.value * l2.unit.baseUnitComparison) == 0;
+    }
+
+}
