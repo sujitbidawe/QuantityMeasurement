7dd5ce55fe87a3b989deeac9b96799ef22d87d63
sujitbidawe <bidawesujit@gmail.com>
2020-04-24 06:01:40 +0530

    added: code to restrict temperature values from being added

--- src/main/java/com/quantitymeasurement/IOperations.java
+++ src/main/java/com/quantitymeasurement/IOperations.java
@@ -5,7 +5,7 @@ public class IOperations implements Operations{
     @Override
     public boolean compare(Quantity quantity1, Quantity quantity2) throws QuantityException {
         if(!quantity1.unit.quantityType.equals(quantity2.unit.quantityType)) {
-            throw new QuantityException(QuantityException.ExceptionType.DIFFERENT_QUANTITY_TYPE, "Different type of quantities ");
+            throw new QuantityException(QuantityException.ExceptionType.DIFFERENT_QUANTITY_TYPE, "Different type of quantities");
         }
         return Double.compare(quantity1.value * quantity1.unit.baseUnitComparison,
                 quantity2.value * quantity2.unit.baseUnitComparison) == 0;
@@ -13,9 +13,12 @@ public class IOperations implements Operations{
 
     @Override
     public double add(Quantity quantity1, Quantity quantity2) throws QuantityException {
-        if (!quantity1.unit.quantityType.equals(quantity2.unit.quantityType))
-            throw new QuantityException(QuantityException.ExceptionType.DIFFERENT_QUANTITY_TYPE, "Different type of quantities ");
-        return quantity1.getValue() * quantity1.unit.baseUnitComparison +
-                quantity2.getValue() * quantity2.unit.baseUnitComparison;
+        if (!quantity1.unit.quantityType.equals(quantity2.unit.quantityType)) {
+            throw new QuantityException(QuantityException.ExceptionType.DIFFERENT_QUANTITY_TYPE, "Different type of quantities");
+        }
+        if (quantity1.unit.quantityType.equals("temperature")){
+            throw new QuantityException(QuantityException.ExceptionType.TEMPERATURE_VALUE, "Temperature cannot be added!");
+        }
+        return quantity1.getValue() * quantity1.unit.baseUnitComparison + quantity2.getValue() * quantity2.unit.baseUnitComparison;
     }
 }
