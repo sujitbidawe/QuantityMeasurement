f35aebacb4879d2845e238488b3182feef662eb3
sujitbidawe <bidawesujit@gmail.com>
2020-04-24 06:03:05 +0530

    added: enum for temperature related exceptions

--- src/main/java/com/quantitymeasurement/QuantityException.java
+++ src/main/java/com/quantitymeasurement/QuantityException.java
@@ -4,7 +4,7 @@ public class QuantityException extends Exception {
     ExceptionType type;
 
     public enum ExceptionType {
-        NEGATIVE_VALUE, DIFFERENT_QUANTITY_TYPE
+        NEGATIVE_VALUE, DIFFERENT_QUANTITY_TYPE, TEMPERATURE_VALUE
     }
 
     public QuantityException(ExceptionType type, String message) {
